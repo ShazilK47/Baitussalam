78c53d8d8c79f6f005bc3456e31038efcabe0fef
ShazilK47 <shazil.akn@gmail.com>
2025-02-23 22:08:52 +0500

    working with functions

---
+++ JavaScript/Class-04/Tasks/02.js
@@ -0,0 +1,13 @@
+// Discount Calculator
+
+let price = parseInt(prompt("Enter the Original Price: "));
+let discountPercentage = parseInt(prompt("Enter Discount Percentage: "));
+function calculateDiscount(price, discountPercentage) {
+  discount = (price * discountPercentage) / 100;
+  discountedPrice = price - discount;
+
+  alert(`Original Price: ${price}, Discounted Price: ${discountedPrice}`);
+}
+
+// calculateDiscount(1000, 30);
+calculateDiscount(price, discountPercentage);
